b3c8e57a023f5149a6c2a8757d32f22c2a297981
Jamie Hewland <jhewland@gmail.com>
2016-05-06 14:47:23 +0200

    Initial Travis file, build script and requirements list

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+DOCKER_IMAGE="python:2"
+REQUIREMENT="$(pwd)/requirements.txt"
+WHEEL_DIR="$(pwd)/wheelhouse"
+NO_DEPS="--no-deps"
+
+while [[ $# > 0 ]]; do
+    key="$1"; shift
+
+    case "$key" in
+        -d|--docker-image)
+            DOCKER_IMAGE="$1"; shift
+            ;;
+        -r|--requirement)
+            REQUIREMENT="$1"; shift
+            ;;
+        -w|--wheel-dir)
+            WHEEL_DIR="$1"; shift
+            ;;
+        --deps)
+            NO_DEPS=""
+            ;;
+        --extra-runargs)
+            EXTRA_RUNARGS="$1"; shift
+            ;;
+        *)
+            # Unknown option
+            echo "Unknown parameter: $key" 1>&2
+            exit 1
+            ;;
+    esac
+done
+
+docker run --rm \
+  -v "$REQUIREMENT":/requirements.txt \
+  -v "$WHEEL_DIR":/wheelhouse \
+  $DOCKER_IMAGE \
+  pip wheel -r /requirements.txt -w /wheelhouse $NO_DEPS $EXTRA_RUNARGS
